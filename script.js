6a956065ddaa56c75da825e97a678b4339b9c9ef
Ritesh Diwan <riteshcph123@rediffmail.com>
2024-04-12 17:31:22 +0530

    test branch

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const container = document.getElementById("container");
-for (let i=0; i<16; i++) {
-    for(j=0; j<16; j++) {
-        const divSquare = document.createElement("div");
-        divSquare.setAttribute('class', "square");
-        container.appendChild(divSquare);
-        divSquare.addEventListener("mouseenter", (e)=> {
-        divSquare.setAttribute("style", "background-color: red;");
-    })
-  }
+for (let i=0; i <256; i++) {
+    const divSquare = document.createElement("div");
+    divSquare.setAttribute('class', "square");
+    container.appendChild(divSquare);
 };
 
